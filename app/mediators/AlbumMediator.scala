aee290dd75ee51965951aca127bf8649258ccf59
Richard <richard.garbi@gmail.com>
2014-01-20 18:02:00 -0600

    Adding the begginings of a mediator

---
+++ app/mediators/AlbumMediator.scala
@@ -0,0 +1,15 @@
+package mediators
+
+import models.Photo
+
+/**
+ * Created by Richard on 1/20/14.
+ */
+class AlbumMediator {
+
+  def getAllPhotosForAnAlbum(id: String): List[Photo] = {
+      return Photo.getAllByAlbumUUID(id)
+  }
+
+
+}
